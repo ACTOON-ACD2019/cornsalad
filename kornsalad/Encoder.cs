95c5bea4e3efb4535cc073284adeaf81af3db786
Dongkyun Yoo <me@sokdak.me>
2019-12-09 03:59:28 +0900

    [Add] encoder base

---
+++ kornsalad/Encoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace kornsalad
+{
+    public class Encoder
+    {
+        Effector[] Effectors { get; set; }
+
+        public Encoder(Effector[] Effectors)
+        {
+            
+        }
+        
+        public void SequentialMerge()
+        {
+
+        }
+
+        public string Encode(string mediaFormat, string outputFile=null)
+        {
+            if (outputFile != null)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
